b37c9249167c0d96884655319cd054386f9b2189
Matteo Piccini <gnappo.1@live.it>
2024-05-29 12:25:46 -0700

    add solution to solution branch

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code below.
+
+// Function using a while loop
+function findItemIndex(items, itemToFind) {
+    let index = 0;
+    while (index < items.length) {
+        if (items[index].name === itemToFind.name && items[index].color === itemToFind.color) {
+            return index;
+        }
+        index++;
+    }
+    return -1;
+}
+
+// Function using a for loop
+function sumArray(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 !== 0) {
+            sum += numbers[i];
+        }
+    }
+    return sum;
+}
+
+// Function using a for...of loop
+function filterByLength(strings, maxLength) {
+    const filteredStrings = [];
+    for (const str of strings) {
+        if (str.length <= maxLength) {
+            filteredStrings.push(str);
+        }
+    }
+    return filteredStrings;
+}
